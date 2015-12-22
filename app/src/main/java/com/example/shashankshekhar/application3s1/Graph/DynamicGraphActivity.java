6e72e7264f7280d98b4b021d7b9c71e9e61cdfb3
shashank <shshnk28@gmail.com>
2015-12-22 13:01:16 +0530

    dummy dynamic XY plot implementation done

--- app/src/main/java/com/example/shashankshekhar/application3s1/Graph/DynamicGraphActivity.java
+++ app/src/main/java/com/example/shashankshekhar/application3s1/Graph/DynamicGraphActivity.java
@@ -17,6 +17,9 @@ import com.example.shashankshekhar.application3s1.R;
 import com.example.shashankshekhar.smartcampuslib.HelperClass.CommonUtils;
 
 import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Observable;
 import java.util.Observer;
 import java.util.Random;
@@ -104,12 +107,13 @@ public class DynamicGraphActivity extends AppCompatActivity {
             @Override
             public void notifyObservers() {
                 setChanged();
-                test =5;
                 super.notifyObservers();
             }
         }
         private static final int SAMPLE_SIZE = 30;
         private int test ;
+        List<Integer> yList = new ArrayList<Integer>(Collections.nCopies(30, 0));
+        List<Integer> xList = new ArrayList<Integer>(Collections.nCopies(30, 0));
         private MyObservable notifier;
         private boolean keepRunning = false;
         Random randomGenerator = new Random();
@@ -124,8 +128,20 @@ public class DynamicGraphActivity extends AppCompatActivity {
         public void run() {
             try {
                 keepRunning = true;
+                test = 0;
                 while(keepRunning) {
-                    Thread.sleep(2000);
+                    Thread.sleep(1000);
+                    int randomInt = randomGenerator.nextInt(100);
+                    test++;
+                    yList.remove(0);
+                    yList.add(29, randomInt);
+                    xList.remove(0);
+                    xList.add(29, test);
+//                    System.out.print(yList);
+                    CommonUtils.printLog("yList: "+yList.toString());
+                    CommonUtils.printLog("xList: "+ xList.toString());
+                    // generate a random int and push it in the queue shift other elements
+                    // then notify the observer
                     CommonUtils.printLog("observers to be notified");
                     notifier.notifyObservers();
                 }
@@ -141,14 +157,14 @@ public class DynamicGraphActivity extends AppCompatActivity {
             if (index >= SAMPLE_SIZE) {
                 throw new IllegalArgumentException();
             }
-            return index;
+            return xList.get(index);
         }
         public Number getY(int series, int index) {
             if (index >= SAMPLE_SIZE) {
                 throw new IllegalArgumentException();
             }
-            int randomInt = randomGenerator.nextInt(100);
-            return randomInt;
+//            int randomInt = randomGenerator.nextInt(100);
+            return yList.get(index);
         }
         public void addObserver(Observer observer) {
             notifier.addObserver(observer);
