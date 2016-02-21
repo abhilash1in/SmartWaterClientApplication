db9dbdd1e9276bbbdb5975d67ffe41ad7c8db2c7
shashank <shshnk28@gmail.com>
2016-02-21 11:27:04 +0530

    nav drawer partial implementation- not fully functional

--- app/src/main/java/com/example/shashankshekhar/application3s1/Settings/SettingsActivity.java
+++ app/src/main/java/com/example/shashankshekhar/application3s1/Settings/SettingsActivity.java
@@ -4,15 +4,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.example.shashankshekhar.application3s1.Map.MapActivity;
 import com.example.shashankshekhar.application3s1.R;
 import com.example.shashankshekhar.smartcampuslib.ServiceAdapter;
 
-public class SettingsActivity extends AppCompatActivity {
+public class SettingsActivity extends MapActivity {
     ServiceAdapter serviceAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+//        super.onCreateDrawer();
         serviceAdapter = ServiceAdapter.getServiceAdapterinstance(getApplicationContext());
     }
     public void bindService (View view) {
@@ -24,4 +26,3 @@ public class SettingsActivity extends AppCompatActivity {
         serviceAdapter.unbindFromService();
     }
 }
-    
